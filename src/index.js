983b1a75ccfcc668d62a2de677efa48774b2d361
SvetlanaReutskaya <lana.reutska@gmail.com>
2022-07-22 12:29:54 +0200

    start project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+alert("AAA");
+
+function myComponent(){
+  const divElement = document.createElement('div');
+  const h2 = document.createElement('h2');
+  h2.innerText = 'My first webpack setup';
+  divElement.appendChild(h2);
+  return divElement;
+}
+document.body.appendChild(myComponent());
